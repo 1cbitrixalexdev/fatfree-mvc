436645f70d9d44d94d73fc01240a11ad43e56120
Stas <1cbitrix.alex.dev@gmail.com>
2019-03-05 13:49:09 +0200

    - added table headers - added Clients model - output client's name

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,18 +9,26 @@
 
 class MainController extends Controller {
 	function render() {
+		// prepare orders ORM
 		$list        = new Orders( $this->db );
 		$orders      = $list->all( null, array( 'limit' => '20', 'offset' => '0' ) );
 		$ordersArray = array();
 		foreach ( $orders as $order ) {
 			$ordersArray[] = $order->cast();
 		}
-
+		//prepare clients ORM
+		$clients = new Clients( $this->db );
+		// prepare orders array
+		foreach ( $ordersArray as $key => $value ) {
+			$ordersArray[ $key ]['start']       = date( "d-m-Y", strtotime( $value['start'] ) );
+			$ordersArray[ $key ]['client_name'] = $clients->getById( $value['client'] )[0]['name'];
+		}
+		// prepare variables
 		$context = array(
 			'list' => $ordersArray,
 			'role' => $this->f3->get( "SESSION.role" )
 		);
-
+		// render
 		echo Controller::twig()->render( 'index.twig', $context );
 	}
 }
